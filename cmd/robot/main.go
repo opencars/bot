9dae2cc783f6da1f2c87903e8a85051c835d3b73
Ali Shanaakh <ashanaakh@gmail.com>
2018-12-30 00:49:10 +0200

    Prepare simple project structure

---
+++ cmd/robot/main.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Ali Shanaakh, github@shanaakh.pro
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+
+package main
+
+import "github.com/shal/robot/internal/bot"
+
+func main() {
+	bot.Run()
+}
---
+++ internal/bot/bot.go
@@ -0,0 +1,12 @@
+// Copyright (C) 2019 Ali Shanaakh, github@shanaakh.pro
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+
+
+package bot
+
+import "fmt"
+
+func Run() {
+	fmt.Print("I am robot")
+}
