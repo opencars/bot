601dcebca80b82d8ac75098c88a51fe9d80813e5
Ali Shanaakh <ashanaakh@gmail.com>
2019-02-21 23:42:58 +0200

    Refactor & Add test to opencars sdk

--- pkg/opencars/transport.go
+++ pkg/opencars/transport.go
@@ -1,16 +1,5 @@
 package opencars
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
-
-type API struct {
-	URI string
-}
-
 type Transport struct {
 	ID                  int    `json:"id"`
 	RegistrationAddress string `json:"registration_address"`
@@ -26,26 +15,4 @@ type Transport struct {
 	Capacity            int    `json:"capacity"`
 	Weight              int    `json:"own_weight"`
 	Number              string `json:"number"`
-}
-
-func (client *API) Search(number string) ([]Transport, error) {
-	query := fmt.Sprintf("%s/transport?number=%s", client.URI, number)
-	response, err := http.Get(query)
-
-	if err != nil {
-		return nil, err
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	transport := make([]Transport, 0)
-	if err = json.Unmarshal(body, &transport); err != nil {
-		return nil, err
-	}
-
-	return transport, nil
 }
